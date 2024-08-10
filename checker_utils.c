a8fa0e70a0388e43418af7f43419da05ff41b8d6
Arif Mohammed <arifmohammedpf77@gmail.com>
2024-08-10 21:05:38 +0530

    bonus utils added

---
+++ checker_utils.c
@@ -0,0 +1,62 @@
+#include "libft.h"
+#include "push_swap_bonus.h"
+#include "push_swap.h"
+
+void	found_error(void)
+{
+	ft_putstr("Error\n");
+	exit(1);
+}
+
+static char	*ft_strcpy(char *dst, const char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	char	*dest;
+	int		i;
+
+	i = ft_strlen(s1);
+	dest = (char *)malloc(sizeof(char) * (i + 1));
+	if (dest == NULL)
+		return (NULL);
+	return (ft_strcpy(dest, s1));
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	i;
+	size_t	j;
+	char	*str;
+
+	i = 0;
+	j = 0;
+	if (!s1 || !s2)
+		return (NULL);
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (str == NULL)
+		return (NULL);
+	while (s1[i] != '\0')
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		str[i + j] = s2[j];
+		j++;
+	}
+	str[i + j] = '\0';
+	return (str);
+}
